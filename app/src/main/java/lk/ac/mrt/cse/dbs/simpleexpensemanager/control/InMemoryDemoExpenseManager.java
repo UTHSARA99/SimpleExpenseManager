c81ebf0af5b50bd6100209bede2560e143a2d5f2
UTHSARA99 <aseluthsara1@gmail.com>
2022-12-13 04:03:34 +0530

    Initial Commit

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/InMemoryDemoExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/InMemoryDemoExpenseManager.java
@@ -16,6 +16,8 @@
 
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
 
+import android.content.Context;
+
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.InMemoryAccountDAO;
@@ -27,26 +29,27 @@ import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
  */
 public class InMemoryDemoExpenseManager extends ExpenseManager {
 
-    public InMemoryDemoExpenseManager() {
-        setup();
+        public InMemoryDemoExpenseManager(Context context) {
+            super(context);
+            setup(context);
+        }
+
+        @Override
+        public void setup(Context context) {
+            /*** Begin generating dummy data for In-Memory implementation ***/
+
+            TransactionDAO inMemoryTransactionDAO = new InMemoryTransactionDAO();
+            setTransactionsDAO(inMemoryTransactionDAO);
+
+            AccountDAO inMemoryAccountDAO = new InMemoryAccountDAO();
+            setAccountsDAO(inMemoryAccountDAO);
+
+            // dummy data
+            Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+            Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+            getAccountsDAO().addAccount(dummyAcct1);
+            getAccountsDAO().addAccount(dummyAcct2);
+
+            /*** End ***/
+        }
     }
-
-    @Override
-    public void setup() {
-        /*** Begin generating dummy data for In-Memory implementation ***/
-
-        TransactionDAO inMemoryTransactionDAO = new InMemoryTransactionDAO();
-        setTransactionsDAO(inMemoryTransactionDAO);
-
-        AccountDAO inMemoryAccountDAO = new InMemoryAccountDAO();
-        setAccountsDAO(inMemoryAccountDAO);
-
-        // dummy data
-        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
-        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
-        getAccountsDAO().addAccount(dummyAcct1);
-        getAccountsDAO().addAccount(dummyAcct2);
-
-        /*** End ***/
-    }
-}
